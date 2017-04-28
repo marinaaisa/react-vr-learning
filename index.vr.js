8353f61b6753bc61d6af0e8908ecf87a2b4ca2f5
MARINA AISA <marina@marinaaisa.com>
2017-04-28 12:28:48 +0200

    Tutorial part 1

--- index.vr.js
+++ index.vr.js
@@ -22,26 +22,25 @@ export default class WelcomeToVR extends React.Component {
           flexDirection: 'column',
           width: 2,
           alignItems: 'stretch',
+          backgroundColor: '#ffffff',
           transform: [{translate: [-1, 1, -5]}],
         }}>
-
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
+          </View>
+          <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+            <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
+          </View>
         </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
-        </View>
-
-      </View>
       </View>
     );
   }
